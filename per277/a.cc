25eacfbab82c3d5f49709e2849a946e398239f05
ofuji <mizuno111222@icloud.com>
2023-07-06 18:25:01 +0900

    277/a

---
+++ per277/a.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int n,x;
+	cin >> n >> x;
+	int k;
+	for(int i=0;i<n;i++){
+		cin >> k;
+		if(k==x){
+			cout << i+1 << endl;
+		}
+	}
+	return 0;
+}
+	
