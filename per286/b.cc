f5a3abe0ab0defa0a030de365b317a3eca50052c
ofuji <mizuno111222@icloud.com>
2023-07-14 19:46:15 +0900

    286/b correction

--- per286/b.cc
+++ per286/b.cc
@@ -7,10 +7,12 @@ int main(){
 	int n;
 	string s;
 	cin >> n >> s;
-	for(int i=0;i<n-1;i++){
+	int n2 = n;
+	for(int i=0;i<n2-1;i++){
 		if(s.substr(i,2)=="na"){ //string.substr(start,length)->nukidashi
 			s.replace(i,2,"nya"); //string.replace(start,length,word) -> okikae
-			i+=2;
+			i+=1;
+			n2++;
 		}
 	}
 	cout << s <<endl;
