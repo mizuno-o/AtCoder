e9c52d48388b37d023b125a5fa1342ad6e375237
ofuji <mizuno111222@icloud.com>
2023-12-05 13:54:29 +0900

    331/a

---
+++ per331/a.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	int M,D;
+	cin >> M >> D;
+	int y,m,d;
+	cin >> y >> m >> d;
+	if(d==D){
+		d=1;
+		if(m==M){
+			m=1;
+			y+=1;
+		}else{
+			m+=1;
+		}
+	}else{
+		d+=1;
+	}
+
+	cout << y << " " << m << " " << d <<endl;
+	return 0;
+}
